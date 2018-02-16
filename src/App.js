2f37d71e922eb1d589b4e629f528f5465a95409e
Joshua Lacey <williamjoshualacey@gmail.com>
2018-02-16 16:41:47 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => (
     <div>
       <nav>
         <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
+        <Link to="/about">AboutTEST</Link>
         <Link to="/blog">Blog</Link>
       </nav>
       <div className="content">
